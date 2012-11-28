733faaeeb1183fe05cd13d7bb19b57311b0ae96e
Ethane Tivano <ethane.tivano@mailoo.org>
2012-11-28 12:24:56 +0100

    font-awesome and css

--- vendor/javascripts/create.js
+++ vendor/javascripts/create.js
@@ -2,4 +2,11 @@
  * CreateJS plugin
  * by Bergius
  */
-//= require create/underscore-min
+//= require jquery
+//= require jquery-ui
+//= require create/deps/underscore-min
+//= require create/deps/backbone-min
+//= require create/deps/vie-min
+//= require create/deps/rangy-core-1.2.3
+//= require create/deps/hallo-min
+//= require create/create-min
