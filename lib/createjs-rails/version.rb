bdb5e0dd3ac73e08439c45eaa66af64ae873d9df
Ethane Tivano <ethane.tivano@mailoo.org>
2012-12-11 11:48:19 +0100

    [js] can't quit page without save

--- lib/createjs-rails/version.rb
+++ lib/createjs-rails/version.rb
@@ -1,3 +1,3 @@
 module CreatejsRails
-  VERSION = "1.0.1"
+  VERSION = "1.0.2"
 end
--- vendor/assets/javascripts/create.js
+++ vendor/assets/javascripts/create.js
@@ -1,7 +1,15 @@
-/*
- * CreateJS plugin
- * by Bergius
- */
+// 
+// Encoding: UTF-8
+//
+// jslint browser: true
+// global  $
+//
+// CreateJS plugin
+// by Henri Bergius
+//
+// repo:    https://github.com/bergie/create
+// website: http://createjs.org/
+//
 //= require jquery
 //= require jquery-ui
 //= require create/deps/underscore-min
@@ -10,3 +18,26 @@
 //= require create/deps/rangy-core-1.2.3
 //= require create/deps/hallo-min
 //= require create/create-min
+//
+
+function setConfirmUnloadPage(on) {
+    window.onbeforeunload = (on) ? unloadMessage : null;
+}
+function unloadMessage() {
+     return 'You have entered new data on this page. If you navigate away from this page without first saving your data, the changes will be lost.'
+}
+$(document).ready(function() { 
+    /* 
+     * On check les modifications sur les champs des formulaires pour activer le blocage de la page
+     * On le désactive au clic sur le bouton enregistrer
+     * Attention : les champs hidden ou disabled ne sont pas concernés
+     */
+    $('body article').bind('midgardeditableenable ', function(event, data) {
+        if (!$(this).hasClass('ignore-validate') && !$(this).hasClass('filter')) {
+            setConfirmUnloadPage(true); 
+        }
+    }); 
+    $('body article').bind('midgardeditabledisable', function(event, data) {
+        setConfirmUnloadPage(false); 
+    }); 
+}); 
