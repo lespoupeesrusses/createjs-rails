a59c3fd0857ce021790e30197f607637957147c4
Ethane Tivano <ethane.tivano@mailoo.org>
2013-06-03 15:58:57 +0200

    update jquery ui version

--- vendor/assets/javascripts/create.js
+++ vendor/assets/javascripts/create.js
@@ -11,7 +11,7 @@
 // website: http://createjs.org/
 //
 //= require jquery-1.8.min
-//= require jquery-ui
+//= require jquery-ui-1.8.18.min
 //= require create/deps/underscore-min
 //= require create/deps/backbone-min
 //= require create/deps/vie-min
@@ -27,6 +27,19 @@ function unloadMessage() {
      return 'You have entered new data on this page. If you navigate away from this page without first saving your data, the changes will be lost.'
 }
 $(document).ready(function() { 
+    $('body article').bind('midgardeditabledisable', function (event, data) {
+        var logo = $('.create-ui-logo #create-ui-toggle-toolbar');
+        if (undefined === logo.attr('href')) {
+            logo.attr('href', 'http://' + window.location.host);
+        }
+        /*
+        logo.click(function (event) {
+            $('.create-ui-toolbar-wrapper').css('opacity', '1');
+            $('.create-ui-toolbar-wrapper').css('display', 'block');
+            console.log($('.create-ui-toolbar-wrapper').css('opacity'));
+        });
+        */
+    });
     /* 
      * On check les modifications sur les champs des formulaires pour activer le blocage de la page
      * On le désactive au clic sur le bouton enregistrer
