9cc69bc162f723248cfc15251656ed611d0aa466
Kora Saccharin <kora.saccharin@mailoo.org>
2013-07-29 14:38:47 +0200

    remove back to home

--- vendor/assets/javascripts/create.js
+++ vendor/assets/javascripts/create.js
@@ -27,19 +27,14 @@ function unloadMessage() {
      return 'You have entered new data on this page. If you navigate away from this page without first saving your data, the changes will be lost.'
 }
 $(document).ready(function() { 
+    /*
     $('body article').bind('midgardeditabledisable', function (event, data) {
         var logo = $('.create-ui-logo #create-ui-toggle-toolbar');
         if (undefined === logo.attr('href')) {
             logo.attr('href', 'http://' + window.location.host);
         }
-        /*
-        logo.click(function (event) {
-            $('.create-ui-toolbar-wrapper').css('opacity', '1');
-            $('.create-ui-toolbar-wrapper').css('display', 'block');
-            console.log($('.create-ui-toolbar-wrapper').css('opacity'));
-        });
-        */
     });
+    */
     /* 
      * On check les modifications sur les champs des formulaires pour activer le blocage de la page
      * On le désactive au clic sur le bouton enregistrer
